9a74d565fded3d2d700fbca15aceb934324f09c2
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-09-30 01:52:29 -0300

    Modulos

---
+++ Modulos/app.js
@@ -0,0 +1,11 @@
+const calculadora = require('./calculadora')
+
+const resultSoma = calculadora.soma(3, 3)
+const resultSub = calculadora.sub(20, 5)
+const resultDiv = calculadora.div(50, 10)
+const resultadoMult = calculadora.mult(5, 3)
+
+console.log(resultSoma)
+console.log(resultSub)
+console.log(resultDiv)
+console.log(resultadoMult)
---
+++ Modulos/calculadora.js
@@ -0,0 +1,22 @@
+function soma(num1, num2) {
+  return num1 + num2
+}
+
+function sub(num1, num2) {
+  return num1 - num2
+}
+
+function mult(num1, num2) {
+  return num1 * num2
+}
+
+function div(num1, num2) {
+  return num1 / num2
+}
+
+module.exports = {
+  soma,
+  sub,
+  mult,
+  div
+}
