02b1ad9ec67f55b5295e12e6e016da550cda94c8
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-11-17 01:25:22 -0300

    Rotas

--- express/index.js
+++ express/index.js
@@ -1,6 +1,14 @@
 const express = require('express') //importando o express
 const app = express() // Iniciando o express
 
+app.get('/', (req, res) => {
+  res.send('Bem vindo ao express')
+})
+
+app.get('/blog', (req, res) => res.send('Bem vindo ao blog'))
+
+app.get('/canal', (req, res) => res.send('Bem vindo ao canal'))
+
 app.listen(4000, function (erro) {
   if (erro) {
     console.log('Ocorreu um erro')
