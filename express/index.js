47b5e5b5dc6b1ac62cddd863bea4c5403d455539
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-11-17 23:47:42 -0300

    Parametros

--- express/index.js
+++ express/index.js
@@ -4,10 +4,15 @@ const app = express() // Iniciando o express
 app.get('/', (req, res) => {
   res.send('Bem vindo ao express')
 })
-
-app.get('/blog', (req, res) => res.send('Bem vindo ao blog'))
+//parametro não obrigatorio
+app.get('/blog/:artigo?', (req, res) => res.send('Bem vindo ao blog'))
 
 app.get('/canal', (req, res) => res.send('Bem vindo ao canal'))
+
+app.get('/ola/:nome', (req, res) => {
+  let nome = req.params.nome
+  res.send('<h1>Oi ' + nome + '</h1>')
+})
 
 app.listen(4000, function (erro) {
   if (erro) {
